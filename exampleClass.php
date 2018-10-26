a7781553292ba89c95764150636cb53e232d1b96
niklas <n.hebisch@go-james.com>
2018-10-26 13:59:59 +0200

    first Commit

---
+++ exampleClass.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niklas
+ * Date: 26.10.18
+ * Time: 13:56
+ */
+namespace Namespace\Unternamespace\Unterunternamespace;
+
+
+class exampleClass
+{
+
+}
